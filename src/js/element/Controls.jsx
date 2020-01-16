b13177e37741cc5f86ad011f04b0c45374a2e8de
Jihoon Yang <lanpai@piyo.cafe>
2020-01-15 22:24:44 -0500

    Fixed request bug

--- src/js/element/Controls.jsx
+++ src/js/element/Controls.jsx
@@ -79,14 +79,14 @@ class Controls extends Component {
                         { toggleButton }
                     </svg>
                 </div>
-                <div className='icon button'>
+                <div className='icon button' style={{ opacity: '0.2' }}>
                     <svg style={{ width: '4vh', height: '4vh' }} viewBox='0 0 24 24'>
-                        <path fill='deeppink' d='M5,5V19H8V5M10,5V19L21,12' />
+                        <path fill='#202020' d='M5,5V19H8V5M10,5V19L21,12' />
                     </svg>
                 </div>
-                <div className='icon button'>
+                <div className='icon button' style={{ opacity: '0.2' }}>
                     <svg style={{ width: '4vh', height: '4vh' }} viewBox='0 0 24 24'>
-                        <path fill='deeppink' d='M12,2A10,10 0 0,0 2,12A10,10 0 0,0 12,22A10,10 0 0,0 22,12A10,10 0 0,0 12,2M12,4A8,8 0 0,1 20,12A8,8 0 0,1 12,20A8,8 0 0,1 4,12A8,8 0 0,1 12,4M9.75,7.82C8.21,7.82 7,9.03 7,10.57C7,12.46 8.7,14 11.28,16.34L12,17L12.72,16.34C15.3,14 17,12.46 17,10.57C17,9.03 15.79,7.82 14.25,7.82C13.38,7.82 12.55,8.23 12,8.87C11.45,8.23 10.62,7.82 9.75,7.82Z' />
+                        <path fill='#202020' d='M12,2A10,10 0 0,0 2,12A10,10 0 0,0 12,22A10,10 0 0,0 22,12A10,10 0 0,0 12,2M12,4A8,8 0 0,1 20,12A8,8 0 0,1 12,20A8,8 0 0,1 4,12A8,8 0 0,1 12,4M9.75,7.82C8.21,7.82 7,9.03 7,10.57C7,12.46 8.7,14 11.28,16.34L12,17L12.72,16.34C15.3,14 17,12.46 17,10.57C17,9.03 15.79,7.82 14.25,7.82C13.38,7.82 12.55,8.23 12,8.87C11.45,8.23 10.62,7.82 9.75,7.82Z' />
                     </svg>
                 </div>
                 <div className='icon' style={{ width: '50%', flex: 4 }}>
--- src/js/element/List.jsx
+++ src/js/element/List.jsx
@@ -75,7 +75,7 @@ class Request extends Component {
                                 </h3>
                             </Marquee>
                         </div>
-                        <div className='button'>
+                        <div className='button' onClick={ () => RequestSong(song.id) }>
                             <svg style={{ width: '2.5vh', height: '2.5vh' }} viewBox='0 0 24 24'>
                                 <path fill='#202020' d='M19,11H15V15H13V11H9V9H13V5H15V9H19M20,2H8A2,2 0 0,0 6,4V16A2,2 0 0,0 8,18H20A2,2 0 0,0 22,16V4A2,2 0 0,0 20,2M4,6H2V20A2,2 0 0,0 4,22H18V20H4V6Z' />
                             </svg>
