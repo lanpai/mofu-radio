b1020eeed94fd1969127df5de932565d4b36e740
lanpai <lan@piyo.cafe>
2020-01-15 17:35:30 -0800

    Improved ui

--- src/js/container/MainInterface.jsx
+++ src/js/container/MainInterface.jsx
@@ -11,26 +11,17 @@ import Controls from '../element/Controls.jsx';
 import Tabs from './Tabs.jsx';
 import Queue from '../element/Queue.jsx';
 import List from '../element/List.jsx';
-import Textbox from '../element/Textbox.jsx';
-import { FetchList } from '../../websocket.js';
 
 const mapStateToProps = state => {
     return {
         currentSong: state.currentSong,
-        stats: state.stats,
-        list: state.list,
-        top: state.top,
-        new: state.new
+        stats: state.stats
     }
 };
 
 class MainInterface extends Component {
     constructor() {
         super();
-    }
-
-    fetchList(e) {
-        FetchList(e.target.value);
     }
 
     render() {
@@ -45,23 +36,7 @@ class MainInterface extends Component {
                 <svg style={{ width: '2.5vh', height: '2.5vh' }} viewBox='0 0 24 24'>
                     <path fill='#202020' d='M18,13H17.32L15.32,15H17.23L19,17H5L6.78,15H8.83L6.83,13H6L3,16V20A2,2 0 0,0 5,22H19A2,2 0 0,0 21,20V16L18,13M17,7.95L12.05,12.9L8.5,9.36L13.46,4.41L17,7.95M12.76,2.29L6.39,8.66C6,9.05 6,9.68 6.39,10.07L11.34,15C11.73,15.41 12.36,15.41 12.75,15L19.11,8.66C19.5,8.27 19.5,7.64 19.11,7.25L14.16,2.3C13.78,1.9 13.15,1.9 12.76,2.29Z' />
                 </svg>,
-                <>
-                    <Textbox placeholder='Filter...' onInput={ this.fetchList } />
-                    <hr />
-                    <List list={ this.props.list } />
-                </>
-            ],
-            [
-                <svg style={{ width: '24px', height: '24px' }} viewBox='0 0 24 24'>
-                    <path fill="#202020" d="M5 16L3 5L8.5 10L12 4L15.5 10L21 5L19 16H5M19 19C19 19.6 18.6 20 18 20H6C5.4 20 5 19.6 5 19V18H19V19Z" />
-                </svg>,
-                <List list={ this.props.top } />
-            ],
-            [
-                <svg style={{ width: '24px', height: '24px' }} viewBox='0 0 24 24'>
-                    <path fill='currentColor' d='M20,4C21.11,4 22,4.89 22,6V18C22,19.11 21.11,20 20,20H4C2.89,20 2,19.11 2,18V6C2,4.89 2.89,4 4,4H20M8.5,15V9H7.25V12.5L4.75,9H3.5V15H4.75V11.5L7.3,15H8.5M13.5,10.26V9H9.5V15H13.5V13.75H11V12.64H13.5V11.38H11V10.26H13.5M20.5,14V9H19.25V13.5H18.13V10H16.88V13.5H15.75V9H14.5V14A1,1 0 0,0 15.5,15H19.5A1,1 0 0,0 20.5,14Z' />
-                </svg>,
-                <List list={ this.props.new } />
+                <List />
             ],
             [
                 <svg style={{ width: '2.5vh', height: '2.5vh' }} viewBox='0 0 24 24'>
@@ -77,7 +52,8 @@ class MainInterface extends Component {
                     <br />
                     <br />
                     <i>made with ♥ by lanpai</i>
-                </div> ]
+                </div>
+            ]
         ];
         return (
             <div className='flex-col'>
--- src/js/element/List.jsx
+++ src/js/element/List.jsx
@@ -4,11 +4,14 @@ import { connect } from 'react-redux';
 import css from '../../css/element/List.scss';
 
 import Marquee from '../container/Marquee.jsx';
-import { RequestSong } from '../../websocket.js';
+import { FetchList, RequestSong } from '../../websocket.js';
 
 const mapStateToProps = state => {
     return {
-        jp: state.jp
+        jp: state.jp,
+        list: state.list,
+        top: state.top,
+        new: state.new
     }
 };
 
@@ -17,13 +20,27 @@ class Request extends Component {
         super();
 
         this.state = {
-            request: []
+            request: [],
+            type: 'list'
         };
+
+        this.setType = this.setType.bind(this);
+    }
+
+    fetchList(e) {
+        FetchList(e.target.value);
+    }
+
+    setType(type) {
+        if (this.state.type === type)
+            this.setState({ type: 'list' });
+        else
+            this.setState({ type: type });
     }
 
     render() {
         let list = [];
-        for (let song of this.props.list) {
+        for (let song of this.props[this.state.type]) {
             let metadata = {
                 artist: song.artist || '',
                 title: song.title || '',
@@ -54,6 +71,20 @@ class Request extends Component {
 
         return (
             <>
+                <div className='textbox' style={ this.props.style }>
+                    <input type='text' placeholder='Filter' onInput={ this.fetchList } />
+                    <div className={ this.state.type === 'top' ? 'button active' : 'button' } onClick={ () => { this.setType('top') } }>
+                        <svg style={{ width: '2.5vh', height: '2.5vh' }} viewBox='0 0 24 24'>
+                            <path fill="#202020" d="M5 16L3 5L8.5 10L12 4L15.5 10L21 5L19 16H5M19 19C19 19.6 18.6 20 18 20H6C5.4 20 5 19.6 5 19V18H19V19Z" />
+                        </svg>
+                    </div>
+                    <div className={ this.state.type === 'new' ? 'button active' : 'button' } onClick={ () => { this.setType('new') } }>
+                        <svg style={{ width: '2.5vh', height: '2.5vh' }} viewBox='0 0 24 24'>
+                            <path fill='currentColor' d='M20,4C21.11,4 22,4.89 22,6V18C22,19.11 21.11,20 20,20H4C2.89,20 2,19.11 2,18V6C2,4.89 2.89,4 4,4H20M8.5,15V9H7.25V12.5L4.75,9H3.5V15H4.75V11.5L7.3,15H8.5M13.5,10.26V9H9.5V15H13.5V13.75H11V12.64H13.5V11.38H11V10.26H13.5M20.5,14V9H19.25V13.5H18.13V10H16.88V13.5H15.75V9H14.5V14A1,1 0 0,0 15.5,15H19.5A1,1 0 0,0 20.5,14Z' />
+                        </svg>
+                    </div>
+                </div>
+                <hr />
                 <div>
                     { list }
                 </div>
